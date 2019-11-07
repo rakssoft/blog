7b364cb3d8d70f89afc507edcf257875661adfdb
Rakssoft <rufedor@mail.ru>
2019-11-07 16:34:40 +0500

    'blogheroku'

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.BloglistView.as_view(), name='home'),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
+
 
 
 ]
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1@b6351isy@5%_ibkivv7zx&nfbr2)njuwf-_cs^$e=afk2b9%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
